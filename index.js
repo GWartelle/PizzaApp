232830a6bed1c980b6691c07955e44c93b10284c
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-08-07 15:43:20 +0200

    2nd exercise answer

--- index.js
+++ index.js
@@ -8,23 +8,31 @@ const menu = [
 const cashInRegister = 100;
 const orderQueue = [];
 
+// #1 exercise
 function addNewPizza(pizzaObj) {
   menu.push(pizzaObj);
 }
 
+// #2 exercise
+// My first answer
+// function placeOrder(pizzaName) {
+//   for (pizza in menu) {
+//     if (pizza[name] === pizzaName) {
+//       cashInRegister += pizza[price];
+//       const orderObj = {
+//         pizza: pizza,
+//         status: "ordered",
+//       };
+//       orderQueue.push(orderObj);
+//       return orderObj;
+//     }
+//   }
+// }
+
 function placeOrder(pizzaName) {
-  for (pizza in menu) {
-    if (pizza[name] === pizzaName) {
-      cashInRegister += pizza[price];
-
-      const orderObj = {
-        pizza: pizza,
-        status: "ordered",
-      };
-
-      orderQueue.push(orderObj);
-
-      return orderObj;
-    }
-  }
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  cashInRegister += selectedPizza.price;
+  const newOrder = { pizza: selectedPizza, status: "ordered" };
+  orderQueue.push(newOrder);
+  return newOrder;
 }
