9d4ec8f352a9ce2c066491892c552d3d598a982d
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-08-07 16:47:11 +0200

    First bug fixes

--- index.ts
+++ index.ts
@@ -1,46 +1,50 @@
 const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
-  ];
-  
-  const cashInRegister = 100;
-  const nextOrderId = 1;
-  const orderQueue = [];
-  
-  // #1 exercise
-  function addNewPizza(pizzaObj) {
-    menu.push(pizzaObj);
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+let cashInRegister = 100;
+let nextOrderId = 1;
+const orderQueue = [];
+
+// #1 exercise
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+}
+
+// #2 exercise
+function placeOrder(pizzaName) {
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  if (!selectedPizza) {
+    console.error(`${pizzaName} does not exist in the menu`);
+    return;
   }
-  
-  // #2 exercise
-  function placeOrder(pizzaName) {
-    const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
-    cashInRegister += selectedPizza.price;
-    const newOrder = {
-      id: nextOrderId++,
-      pizza: selectedPizza,
-      status: "ordered",
-    };
-    orderQueue.push(newOrder);
-    return newOrder;
-  }
-  
-  // #3 exercise
-  function completeOrder(orderId) {
-    const order = orderQueue.find((order) => order.id === orderId);
-    order.status = "completed";
-    return order;
-  }
-  
-  addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
-  addNewPizza({ name: "BBQ Chicken", price: 12 });
-  addNewPizza({ name: "Spicy Sausage", price: 11 });
-  
-  placeOrder("Chicken Bacon Ranch");
-  completeOrder("1");
-  
-  console.log("Menu:", menu);
-  console.log("Cash in register:", cashInRegister);
-  console.log("Order queue:", orderQueue);
+  cashInRegister += selectedPizza.price;
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
+}
+
+// #3 exercise
+function completeOrder(orderId) {
+  const order = orderQueue.find((order) => order.id === orderId);
+  order.status = "completed";
+  return order;
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ Chicken", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 11 });
+
+placeOrder("Chicken Bacon Ranch");
+completeOrder("1");
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
