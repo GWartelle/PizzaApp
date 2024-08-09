07631ebba0dc76600c698b0b239b16f9fbaaccb1
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-08-09 13:33:28 +0200

    3rd test creation

---
+++ test3.js
@@ -0,0 +1,18 @@
+"use strict";
+var gameScores = [14, 21, 33, 42, 59];
+var favoriteThings = [
+    "raindrops on roses",
+    "whiskers on kittens",
+    "bright copper kettles",
+    "warm woolen mittens",
+];
+var voters = [
+    { name: "Alice", age: 42 },
+    { name: "Bob", age: 77 },
+];
+function getLastItem(array) {
+    return array[array.length - 1];
+}
+console.log(getLastItem(gameScores));
+console.log(getLastItem(favoriteThings));
+console.log(getLastItem(voters));
