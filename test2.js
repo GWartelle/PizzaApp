adc5c627bd047da86d34741775e28ee0134fcaf6
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-08-09 12:51:19 +0200

    New test file update

--- test2.js
+++ test2.js
@@ -1,13 +1,51 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var nextUserId = 1;
 var users = [
-    { username: "john_doe", role: "member" },
-    { username: "jane_doe", role: "admin" },
-    { username: "guest_user", role: "guest" },
+    { id: nextUserId++, username: "john_doe", role: "member" },
+    { id: nextUserId++, username: "jane_doe", role: "admin" },
+    { id: nextUserId++, username: "guest_user", role: "guest" },
+    { id: nextUserId++, username: "alice_jones", role: "member" },
 ];
-function fetchUserDetails(username) {
-    var user = users.find(function (user) { return user.username === username; });
-    if (!user) {
-        throw new Error("User with username ".concat(username, " not found"));
+function fetchUserDetails(identifier) {
+    var user;
+    if (typeof identifier === "string") {
+        user = users.find(function (user) { return user.username.toLowerCase() === identifier.toLowerCase(); });
+        if (!user) {
+            throw new Error("User with username ".concat(identifier, " not found"));
+        }
+    }
+    else if (typeof identifier === "number") {
+        user = users.find(function (user) { return user.id === identifier; });
+        if (!user) {
+            throw new Error("User with id ".concat(identifier, " not found"));
+        }
+    }
+    else {
+        throw new TypeError("Parameter 'identifier' must be either a string or a number");
     }
     return user;
 }
+function updateUser(id, updates) {
+    var user = fetchUserDetails(id);
+    Object.assign(user, updates);
+}
+function addNewUser(newUser) {
+    var user = __assign({ id: nextUserId++ }, newUser);
+    users.push(user);
+    return user;
+}
+console.log(users);
+updateUser(1, { username: "new_john_doe" });
+addNewUser({ username: "joe_schmoe", role: "member" });
+console.log(users);
--- test2.ts
+++ test2.ts
@@ -1,20 +1,59 @@
 type UserRole = "guest" | "member" | "admin";
 
 type User = {
+  id: number;
   username: string;
   role: UserRole;
 };
 
+type UpdatedUser = Partial<User>;
+
+let nextUserId = 1;
+
 const users: User[] = [
-  { username: "john_doe", role: "member" },
-  { username: "jane_doe", role: "admin" },
-  { username: "guest_user", role: "guest" },
+  { id: nextUserId++, username: "john_doe", role: "member" },
+  { id: nextUserId++, username: "jane_doe", role: "admin" },
+  { id: nextUserId++, username: "guest_user", role: "guest" },
+  { id: nextUserId++, username: "alice_jones", role: "member" },
 ];
 
-function fetchUserDetails(username: string): User {
-  const user = users.find((user) => user.username === username);
-  if (!user) {
-    throw new Error(`User with username ${username} not found`);
+function fetchUserDetails(identifier: string | number): User {
+  let user;
+  if (typeof identifier === "string") {
+    user = users.find(
+      (user) => user.username.toLowerCase() === identifier.toLowerCase()
+    );
+    if (!user) {
+      throw new Error(`User with username ${identifier} not found`);
+    }
+  } else if (typeof identifier === "number") {
+    user = users.find((user) => user.id === identifier);
+    if (!user) {
+      throw new Error(`User with id ${identifier} not found`);
+    }
+  } else {
+    throw new TypeError(
+      "Parameter 'identifier' must be either a string or a number"
+    );
   }
   return user;
 }
+
+function updateUser(id: number, updates: UpdatedUser): void {
+  const user = fetchUserDetails(id);
+  Object.assign(user, updates);
+}
+
+function addNewUser(newUser: Omit<User, "id">): User {
+  const user: User = {
+    id: nextUserId++,
+    ...newUser,
+  };
+  users.push(user);
+  return user;
+}
+
+console.log(users);
+updateUser(1, { username: "new_john_doe" });
+addNewUser({ username: "joe_schmoe", role: "member" });
+console.log(users);
